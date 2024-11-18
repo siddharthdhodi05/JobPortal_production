ddef2f6b86129b9e9aa04f027dafd34ece08ce9b
siddharthdhodi05 <siddharth.dhodi.work@gmail.com>
2024-11-18 16:08:44 +0530

    url change

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:'https://jobportal-production.onrender.com',
     credentials:true
 }
 
